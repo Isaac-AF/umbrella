00c498916b45df2a898031d9dbf9c8ed59dd980d
Isaac-AF <Isaac.Frank@ChicagoBooth.edu>
2025-04-14 06:01:03 +0000

    Full Solution with ASCII Chart

--- umbrella.rb
+++ umbrella.rb
@@ -10,7 +10,7 @@ print "Where are you located?\n"
 
 location = gets.chomp.gsub(" ","%20")
 
-print "Checking the weather at #{location}...\n"
+print "Checking the weather at #{location}...\n\n"
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{google_api_key}"
 
@@ -22,4 +22,57 @@ longitude = parsed_response.fetch("results").at(0).fetch("geometry").fetch("loca
 
 print "The coordinates of your location are #{latitude}, #{longitude}.\n"
 
-#print "It is currently #{temperature}"
+pirate_url = "https://api.pirateweather.net/forecast/#{pirateweather_api_key}/#{latitude},#{longitude}"
+
+response = HTTP.get(pirate_url)
+parsed_response = JSON.parse(response.to_s)
+
+current_temp = parsed_response.fetch("currently").fetch("temperature")
+
+print "It is currently #{current_temp}°F.\n"
+
+next_hour_summary = parsed_response.fetch("minutely").fetch("summary")
+
+print "Next hour: #{next_hour_summary}.\n\n"
+
+hourly_precipitation = []
+
+12.times do |hour|
+  hourly_precipitation.push(parsed_response.fetch("hourly").fetch("data").at(hour).fetch("precipProbability"))
+end
+
+print "Precipitation probability vs. Hours from now"
+
+steps = 0
+max_precip = (hourly_precipitation.max * 100).round(-1)
+print "\n"
+
+while steps <= max_precip/5.0
+  if max_precip - steps * 5 == 5 || max_precip - steps * 5 == 0
+    print " "
+    print max_precip - steps * 5
+  else
+    print max_precip - steps * 5
+  end
+  print "|"
+
+  hourly_precipitation.each do |hour|
+    if hour * 100 > max_precip - ((steps + 1) * 5)
+      print "*  "
+    else
+      print "   "
+    end
+  end
+  print "\n"
+  steps = steps + 1
+end
+
+print "   ----------------------------------\n"
+print "   1  2  3  4  5  6  7  8  9 10 11 12\n"
+
+if max_precip > 10
+  print "You might want to carry an umbrella!\n"
+else
+  print "No need to bring an umbrella today.\n"
+end
+#print hourly_precipitation
